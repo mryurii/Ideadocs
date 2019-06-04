dd2e45f947433f946a03105f19275e7ac2b58e12
Yurii <yuriy.yakymovych@gmail.com>
2019-06-04 04:56:09 +0300

    Add plpgsql

--- ideaboard-api/db/schema.rb
+++ ideaboard-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_27_035210) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "boardtitle"
     t.datetime "created_at", null: false
